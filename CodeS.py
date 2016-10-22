3000d919280b8ff6281735469690036be54c4f3d
SJJCoding <SJJCODING@gmail.com>
2016-10-21 17:52:15 -0700

    Update CodeS.py

--- CodeS.py
+++ CodeS.py
@@ -15,13 +15,34 @@ class Circle:
     def __init__(self):
         self.radius = 50
         self.center_point = (100, 100)
+    def update_x(self, shift_x):
+        self.center_point = (
+        self.center_point[0] + shift_x,
+        self.center_point[1]
+            )
+    def update_y(self, shift_y):
+        self.center_point = (
+        self.center_point[0],
+        self.center_point[1] + shift_y, 
+            )    
     
     
-
 class Character:
+    key_map = {
+        "right": 39,
+        "left": 37,
+        "up": 38,
+        "down": 40,
+    
+    }
+
     def __init__(self):
         self.circle_shape = Circle()
         self.shape_attributes = ShapeAttributes()
+        self.movement = 10
+        self.movement2 = -10
+        self.movement3 = 10
+        self.movement4 = -10
     def draw_me(self, canvas):
         
         canvas.draw_circle(self.circle_shape.center_point,
@@ -32,7 +53,15 @@ class Character:
                            )
                            
     def move(self, key):
-          print key
+        if key == 39:
+            self.circle_shape.update_x(self.movement)
+        if key == 37:
+            self.circle_shape.update_x(self.movement2)
+        if key == 40:
+            self.circle_shape.update_y(self.movement3)
+        if key == 38:
+            self.circle_shape.update_y(self.movement4)    
+        print key
                            
                            
         
@@ -56,4 +85,3 @@ frame = simplegui.create_frame("Home", 500, 500)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(cliq.move)
 frame.start()
-
