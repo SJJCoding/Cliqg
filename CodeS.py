e0ca9bf4e19c9e826bd3339e1c6120a624aac3ad
SJJCoding <SJJCODING@gmail.com>
2016-11-18 17:55:34 -0800

    Update CodeS.py

--- CodeS.py
+++ CodeS.py
@@ -1,87 +1,103 @@
 
 import simplegui
 
+frame = simplegui.create_frame("Home", 500, 500)
+
 class ShapeAttributes:
+    
     def __init__(self):
-        self.line_width = 100
-        self.line_color = "red"
-        self.fill_color = "red"
-        
-        
-    
-    
+        self.line_width = 5
+        self.line_color = "white"
+        self.fill_color = "black"
 
 class Circle:
+    
     def __init__(self):
         self.radius = 50
         self.center_point = (100, 100)
+        
     def update_x(self, shift_x):
         self.center_point = (
-        self.center_point[0] + shift_x,
-        self.center_point[1]
-            )
+            self.center_point[0] + shift_x,
+            self.center_point[1]
+        )
+        
     def update_y(self, shift_y):
         self.center_point = (
-        self.center_point[0],
-        self.center_point[1] + shift_y, 
-            )    
-    
-    
+            self.center_point[0],
+            self.center_point[1] + shift_y
+        )
+
 class Character:
+
     key_map = {
-        "right": 39,
         "left": 37,
         "up": 38,
-        "down": 40,
-    
+        "right": 39,
+        "down": 40
     }
+    
+    move_dist = 4
+    vel = [move_dist, 0]
 
     def __init__(self):
         self.circle_shape = Circle()
         self.shape_attributes = ShapeAttributes()
-        self.movement = 10
-        self.movement2 = -10
-        self.movement3 = 10
-        self.movement4 = -10
+        
     def draw_me(self, canvas):
+        self.circle_shape.center_point = (
+        self.circle_shape.center_point[0] + Character.vel[0],    
+        self.circle_shape.center_point[1] + Character.vel[1]
+        )    
+        canvas.draw_circle(
+            self.circle_shape.center_point,
+            self.circle_shape.radius,
+            self.shape_attributes.line_width,
+            self.shape_attributes.line_color,
+            self.shape_attributes.fill_color
+        )
         
-        canvas.draw_circle(self.circle_shape.center_point,
-                           self.circle_shape.radius,
-                           self.shape_attributes.line_width,
-                           self.shape_attributes.line_color,
-                           self.shape_attributes.fill_color
-                           )
-                           
     def move(self, key):
-        if key == 39:
-            self.circle_shape.update_x(self.movement)
-        if key == 37:
-            self.circle_shape.update_x(self.movement2)
-        if key == 40:
-            self.circle_shape.update_y(self.movement3)
-        if key == 38:
-            self.circle_shape.update_y(self.movement4)    
-        print key
-                           
-                           
-        
-              
-        #canvas.draw_circle((200, 200) , 50, 100,   "red")
+        if key in Character.key_map.values():
+            if key == Character.key_map["right"]:
+                print "move right"
+                Character.vel = [Character.move_dist, 0]
+            if key == Character.key_map["left"]:
+                print "move left"
+                Character.vel = [-Character.move_dist, 0]
+            if key == Character.key_map["up"]:
+                print "move up"
+                Character.vel = [0, -Character.move_dist]
+            if key == Character.key_map["down"]:
+                print "move down"
+                Character.vel = [0, Character.move_dist]
+cliq = Character()                
+               
+
+
+
+#========add this code==================
+def rect_coords (length, height, startpos = (0, 0)) :
     
+    return [
+        (startpos[0], startpos[1]),
+        (startpos[0], startpos[1] + height),
+        (startpos[0] + length, startpos[1] + height),
+        (startpos[0] + length, startpos[1])  
+    ]
 
-cliq = Character()
-#print type(cliq)
+box = rect_coords(500, 500)
+#===============================
 
-# Handler to draw on canvas
+
+
 def draw(canvas):
     cliq.draw_me(canvas)
+    
+    #===========add this line===============
+    canvas.draw_polygon(box, 20, "white")
+    
 
-# Create a frame and assign callbacks to event handlers
-frame = simplegui.create_frame("Home", 500, 500)
-
-
-
-# Start the frame animation
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(cliq.move)
 frame.start()
